34d04f3b28cbdbe6d1f754d473de087ee1ed0ef0
Dominik <dominik.euba@gmx.de>
2020-11-10 19:54:44 +0100

    adding the calculation of the circle
    in this step a function ´area_of _circle´ is created. furthermore the user input for the radius is implemented and the command for the calculation itself.

--- calculator.py
+++ calculator.py
@@ -4,8 +4,16 @@ Created on Tue Nov 10 12:44:14 2020
 @author: Dominik Euba
 """
 
+import math
+
+# function for calculating the area of a circle
 def area (h, b):
     return (h*b) / 2
+
+#Function that defines an area of a circle
+def area_of_circle(r):
+    return  r**2 * math.pi
+    
 
 #print to console 
 print("this application calculates the area of a triangle:")
@@ -16,8 +24,25 @@ b =  float(input('enter base side of the triangle in cm: '))
 
 #calculating area of the triangle
 trianglearea = area(h,b)
+#print solution to the console
+print("the triangle has an area of: " , round(trianglearea,2), "cm^2")
+
+#next step calculating circle area
+print("in the next step this application calculates the area of a circle: ")
+
+#user input for the radius
+r =  float(input('enter radius of the circle in cm: '))
+
+#calculating area of the circle
+circlearea = area_of_circle(r)
 
 #print solution to the console
 print("the circle has an area of: " , round(circlearea,2), "cm^2")
 
-
+
+
+
+
+
+
+
