e68ef3194e52479b2622eecf6c9e01d8953f3700
Dominik <dominik.euba@gmx.de>
2020-11-10 18:55:09 +0100

    print to console and user input
    adding a description, what the traingle calculator does, and adding user inputs for calculation.

--- calculator.py
+++ calculator.py
@@ -7,3 +7,12 @@ Created on Tue Nov 10 12:44:14 2020
 def area (h, b):
     return (h*b) / 2
 
+#print to console 
+print("this application calculates the area of a triangle:")
+
+#user input for height and side length
+h =  float(input('enter height of the triangle in cm. '))
+b =  float(input('enter base side of the triangle in cm: '))
+
+
+
