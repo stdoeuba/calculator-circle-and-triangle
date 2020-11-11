f4ebfecc5448474391ae74239de64db1ea1e5256
stdoeuba <73879839+stdoeuba@users.noreply.github.com>
2020-11-11 20:14:21 +0100

    Update test_function.py

--- test_function.py
+++ test_function.py
@@ -10,8 +10,16 @@ from calculator import area
 
 def test_area ():
 	assert area(h, b) == 5
-	
-	
 """
+	
+
+from calculator import circle_of_area
+from math import pi
+
+def test_area_of_circle():
+    assert area_of_circle(5) == 25*pi
+	
+	
+
 	
 
