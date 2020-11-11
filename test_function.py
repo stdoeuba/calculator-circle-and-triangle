2b34b2c22b49f724451bd3102326e90c637ef4c0
stdoeuba <73879839+stdoeuba@users.noreply.github.com>
2020-11-11 19:46:58 +0100

    Update test_function.py

--- test_function.py
+++ test_function.py
@@ -1,12 +1,13 @@
 
 from foo import foo
-#from calculator import area
 
 def test_foo():
 	assert foo(5) == 6
 	
 
 """
+from calculator import area
 def test_area (): 
     	assert area(5,2) == 5
+	
 """
